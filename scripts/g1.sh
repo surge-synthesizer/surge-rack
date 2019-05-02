5ebf1835ba78ae5e7db5267de935939cabd172ca
Paul <baconpaul@users.noreply.github.com>
2019-05-02 18:17:32 -0400

    SurgeStorage reads from the plugin (#53)
    SurgeStorage reads configuration.xml and wavetables from
    the plugin zip now, not from the surge install you happen
    to have.
    
    Closes #25

--- scripts/g1.sh
+++ scripts/g1.sh
@@ -10,4 +10,13 @@ if [ ! -f build/rack1.info ]; then
 	echo date > build/rack1.info
 fi
 
-RACK_DIR=${RACK_DIR} make -j 4 -k go
+RACK_DIR=${RACK_DIR} make -j 4 -k dist
+cp dist/SurgeRack-1.0.0-mac.zip ${RACK_DIR}/plugins
+pushd ${RACK_DIR}/plugins
+rm -rf SurgeRack
+unzip SurgeRack-1.0.0-mac.zip
+cd ${RACK_DIR}
+ls ./plugins/SurgeRack/surge-data
+make run
+popd
+
