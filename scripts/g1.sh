f3955d1843246251355b36a1baba0404efb1f02b
Paul <paul@pwjw.com>
2019-05-08 09:24:17 -0400

    Implement Presets
    This commit implements factory preset loading for each module in a
    generic way in Rack V1. Basically each module (a SurgeModuleCommon
    subclass with a getName()) scans "res/presets/getName()" for presets
    at the outset. So preset creation is simply creating that directory
    and pushing vcvm files. To get started I included @oddy.o.trax's OSC
    inits.

--- scripts/g1.sh
+++ scripts/g1.sh
@@ -10,16 +10,18 @@ if [ ! -f build/rack1.info ]; then
 	echo date > build/rack1.info
 fi
 
-RACK_DIR=${RACK_DIR} make -j 4 -k all || exit 2
-cp plugin.dylib ${RACK_DIR}/plugins/SurgeRack
-cd ${RACK_DIR}
+#RACK_DIR=${RACK_DIR} make -j 4 -k all || exit 2
+#cp plugin.dylib ${RACK_DIR}/plugins/SurgeRack
+#cd ${RACK_DIR}
 #lldb -- ./Rack -d 
-./Rack -d 
-exit 0
+#./Rack -d 
+#exit 0
+RACK_DIR=${RACK_DIR} make -j 4 -k dist || exit 2
 cp dist/SurgeRack-1.0.0-mac.zip ${RACK_DIR}/plugins
-pushd ${RACK_DIR}/plugins
+cd ${RACK_DIR}/plugins
 rm -rf SurgeRack
 unzip SurgeRack-1.0.0-mac.zip
 ls ./plugins/SurgeRack/surge-data
-popd
+cd ${RACK_DIR}
+./Rack -d 
 
