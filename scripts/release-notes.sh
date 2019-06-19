4bfb9e296c38b6fa5cab671a6481959a5fc572b7
Paul <baconpaul@users.noreply.github.com>
2019-06-19 11:46:40 -0400

    Move to Rack 1.0.0 SDK (#228)

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -8,7 +8,7 @@ to master. As such it is the latest version of our code and may be
 unstable, unusable, unsuitable for human consumption, and so on.
 
 These assets were built against
-https://vcvrack.com/downloads/Rack-SDK-1.dev.b4bd09a.zip
+https://vcvrack.com/downloads/Rack-SDK-1.0.0.zip
 
 The build date and most recent commits are:
 
