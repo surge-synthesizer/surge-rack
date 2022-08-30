35373519efc190662ced0da73d33028e2b4eab09
Paul Walker <paul@pwjw.com>
2022-08-30 12:54:40 -0400

    Use Visible rather than AddRemove

--- src/SurgeVCF.cpp
+++ src/SurgeVCF.cpp
@@ -23,6 +23,7 @@ SurgeVCFWidget::SurgeVCFWidget(SurgeVCFWidget::M *module) : SurgeModuleWidgetCom
     bg->moduleSpecificDraw = [this](NVGcontext *vg) { this->moduleBackground(vg); };
     addChild(bg);
 
+<<<<<<< Updated upstream
     addParam(rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 20), module, M::A_PARAM));
     addParam(
         rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 80), module, M::A_MOD1_DEPTH));
@@ -31,6 +32,67 @@ SurgeVCFWidget::SurgeVCFWidget(SurgeVCFWidget::M *module) : SurgeModuleWidgetCom
     addParam(
         rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 140), module, M::A_MOD2_DEPTH));
     addInput(rack::createInput<rack::PJ301MPort>(rack::Vec(100,140), module, M::MOD_2));
+=======
+    // knobA = SurgeModulatableKnob::create(rack::Vec(20, 20), module, M::A_PARAM);
+    knobA = rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 20), module, M::A_PARAM);
+    modA_1 = SurgeModulatableRing::create(rack::Vec(20, 20), module, M::A_MOD1_DEPTH);
+    modA_1->underlyerParamWidget = knobA;
+    modA_2 = SurgeModulatableRing::create(rack::Vec(20, 20), module, M::A_MOD2_DEPTH);
+    modA_2->underlyerParamWidget = knobA;
+    addChild(knobA);
+    modA_1->setVisible(false);
+    modA_2->setVisible(false);
+    
+    addChild(modA_1);
+    addChild(modA_2);
+
+    addParam(rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 80), module, M::A_PARAM));
+    addInput(rack::createInput<rack::PJ301MPort>(rack::Vec(100, 80), module, M::MOD_1));
+
+    toggle1 = rack::createWidget<SurgeUIOnlyToggleButton>(rack::Vec(75, 80));
+    toggle1->pressedState = false;
+    toggle1->onToggle = [this](bool isOn) {
+        if (isOn)
+        {
+            toggle2->pressedState = false;
+            toggle2->bdw->dirty = true;
+
+            modA_1->setVisible(true);
+            modA_2->setVisible(false);
+            modA_1->bdw->dirty = true;
+        }
+        else
+        {
+            modA_1->setVisible(false);
+            modA_2->setVisible(false);
+        }
+    };
+    addChild(toggle1);
+
+    addParam(
+        rack::createParam<rack::RoundBigBlackKnob>(rack::Vec(20, 140), module, M::A_MOD2_DEPTH));
+    addInput(rack::createInput<rack::PJ301MPort>(rack::Vec(100, 140), module, M::MOD_2));
+
+    toggle2 = rack::createWidget<SurgeUIOnlyToggleButton>(rack::Vec(75, 140));
+    toggle2->pressedState = false;
+    toggle2->onToggle = [this](bool isOn) {
+        if (isOn)
+        {
+            toggle1->pressedState = false;
+            toggle1->bdw->dirty = true;
+
+            modA_1->setVisible(false);
+            modA_2->setVisible(true);
+            modA_2->bdw->dirty = true;
+        }
+        else
+        {
+            modA_1->setVisible(false);
+            modA_2->setVisible(false);
+        }
+    };
+    addChild(toggle2);
+>>>>>>> Stashed changes
 
     addOutput(rack::createOutput<rack::PJ301MPort>(rack::Vec(20, 200), module, M::SIGNAL_OUT));
 }
