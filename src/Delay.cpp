31b228f2a5054bab403bd32cb542bc6e946529b8
Paul <baconpaul@users.noreply.github.com>
2022-10-02 19:06:40 -0400

    More work on the standalone delay effect (#417)
    Still not done. But better!

--- src/Delay.cpp
+++ src/Delay.cpp
@@ -23,9 +23,116 @@ DelayWidget::DelayWidget(DelayWidget::M *module) : XTModuleWidget()
 {
     setModule(module);
 
+    for (auto &uk : underKnobs)
+        uk = nullptr;
+
+    for (auto &ob : overlays)
+        for (auto &o : ob)
+            o = nullptr;
+
     box.size = rack::Vec(rack::app::RACK_GRID_WIDTH * numberOfScrews, rack::app::RACK_GRID_HEIGHT);
     auto bg = new widgets::Background(box.size, "Delay", "fx", "BlankNoDisplay");
     addChild(bg);
+
+    int row{0}, col{0};
+    std::map<int, std::string> nm;
+    nm[M::TIME_L] = "LEFT";
+    nm[M::TIME_R] = "RIGHT";
+    nm[M::TIME_S] = "SENS";
+    nm[M::FEEDBACK] = "FBACK";
+    nm[M::CROSSFEED] = "CROSSFD";
+    nm[M::LOCUT] = "LOCUT";
+    nm[M::HICUT] = "HICUT";
+    nm[M::MODRATE] = "MRATE";
+    nm[M::MODDEPTH] = "MDEPT";
+    nm[M::MIX] = "MIX";
+
+    for (int p = M::TIME_L; p <= M::MIX; ++p)
+    {
+        if (p == M::FEEDBACK)
+        {
+            row = 1;
+            col = 0;
+        }
+        if (p == M::MODRATE)
+        {
+            row = 2;
+            col = 1;
+        }
+        auto xcmm = columnCenters_MM[col];
+        auto ycmm = 71 - (2 - row) * 16;
+        auto pos = rack::mm2px(rack::Vec(xcmm, ycmm));
+        auto knob = rack::createParamCentered<widgets::Knob9>(pos, module, p);
+        if (knob)
+        {
+            addChild(knob);
+            auto boxx0 = xcmm - columnWidth_MM * 0.5;
+            auto boxy0 = ycmm + 4.073;
+
+            auto p0 = rack::mm2px(rack::Vec(boxx0, boxy0));
+            auto s0 = rack::mm2px(rack::Vec(columnWidth_MM, 5));
+            auto lab = widgets::Label::createWithBaselineBox(p0, s0, nm[p]);
+            addChild(lab);
+
+            underKnobs[p] = knob;
+
+            for (int m = 0; m < M::n_mod_inputs; ++m)
+            {
+                auto radius = rack::mm2px(knob->knobSize_MM + 2 * widgets::KnobN::ringWidth_MM);
+                int id = M::modulatorIndexFor(p, m);
+                auto *k = widgets::ModRingKnob::createCentered(pos, radius, module, id);
+                overlays[p][m] = k;
+                k->setVisible(false);
+                k->underlyerParamWidget = knob;
+                knob->modRings.insert(k);
+                addChild(k);
+            }
+        }
+        col++;
+        if (col == 4)
+        {
+            col = 0;
+            row++;
+        }
+    }
+
+    {
+        auto xcmm = columnCenters_MM[0];
+        auto ycmm = 71 - (2 - 2) * 16;
+        auto pos = rack::mm2px(rack::Vec(xcmm, ycmm));
+
+        auto port = rack::createInputCentered<widgets::Port>(
+            rack::mm2px(rack::Vec(xcmm, ycmm + verticalPortOffset_MM)), module, M::INPUT_CLOCK);
+        addChild(port);
+
+        auto boxx0 = xcmm - columnWidth_MM * 0.5;
+        auto boxy0 = ycmm + 8.573 - 5;
+
+        auto p0 = rack::mm2px(rack::Vec(boxx0, boxy0));
+        auto s0 = rack::mm2px(rack::Vec(columnWidth_MM, 5));
+        auto lab = widgets::Label::createWithBaselineBox(p0, s0, "CLOCK");
+
+#if SORTED_OUT_BPM
+        lab->hasDynamicLabel = true;
+        lab->module = module;
+        lab->dynamicLabel = [](modules::XTModule *m) -> std::string {
+            if (!m)
+                return "CLOCK";
+            auto fxm = static_cast<FX<fxType> *>(m);
+            typedef modules::ClockProcessor<FX<fxType>> cp_t;
+
+            if (fxm->clockProc.clockStyle == cp_t::QUARTER_NOTE)
+            {
+                return "CLOCK";
+            }
+            else
+            {
+                return "BPM";
+            }
+        };
+#endif
+        addChild(lab);
+    }
 
     for (int i = 0; i < M::n_mod_inputs; ++i)
     {
@@ -70,7 +177,7 @@ DelayWidget::DelayWidget(DelayWidget::M *module) : XTModuleWidget()
                                                           M::DELAY_MOD_INPUT + i));
     }
 
-    int col = 0;
+    col = 0;
     for (auto p : {M::INPUT_L, M::INPUT_R})
     {
         auto yp = inputRowCenter_MM;
