771a82a7da2d2c1d4c91ae9c328225f514bc9563
Paul Walker <paul@pwjw.com>
2022-11-15 21:30:10 -0500

    FPU Guard isn't needed
    On discord, Oct 17, Vortico told me:
    
    DSP threads run with
        _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
        _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
        _MM_SET_ROUNDING_MODE(_MM_ROUND_NEAREST);
    
    regardless of standalone or plugin.
    
    and the FPU guard here seems to take a lot of CPU in high thread
    rosetta situations. So don't do it and rely on the rac environment.

--- src/VCF.h
+++ src/VCF.h
@@ -312,7 +312,7 @@ struct VCF : public modules::XTModule
 
     void process(const typename rack::Module::ProcessArgs &args) override
     {
-        auto fpuguard = sst::plugininfra::cpufeatures::FPUStateGuard();
+        // auto fpuguard = sst::plugininfra::cpufeatures::FPUStateGuard();
 
         auto ftype = (sst::filters::FilterType)(int)(std::round(params[VCF_TYPE].getValue()));
         auto fsubtype =
