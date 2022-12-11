0a60b301f721c05cdf8109bbece01e43a44865ab
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2022-12-11 02:34:13 +0100

    set ARCH_X64 define with generator expression

--- RackSDK.cmake
+++ RackSDK.cmake
@@ -23,12 +23,13 @@ set(CMAKE_CXX_STANDARD 11)
 
 # Since the plugin's compiler could be a different version than Rack's compiler, link libstdc++ and libgcc statically to avoid ABI issues.
 add_link_options($<$<CXX_COMPILER_ID:GNU>:-static-libstdc++> $<$<PLATFORM_ID:Linux>:-static-libgcc>)
+add_compile_definitions($<IF:$<STREQUAL:${CMAKE_OSX_ARCHITECTURES},arm64>,ARCH_ARM64,ARCH_X64>)
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
   if (NOT MINGW)
     message(FATAL_ERROR "Rack plugin development environment is only supported for MSYS2/MinGW")
   endif ()
-  add_compile_definitions(ARCH_WIN ARCH_X64)
+  add_compile_definitions(ARCH_WIN)
   add_compile_definitions(_USE_MATH_DEFINES)
   # disabled - causes surge-rack/surge/libs/sst/sst-plugininfra/src/misc_windows.cpp:20:20: error: cannot convert 'const char*' to 'LPCWSTR' {aka 'const wchar_t*'}
   # see also https://gcc.gnu.org/onlinedocs/gcc/x86-Windows-Options.html
@@ -41,18 +42,16 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   message(STATUS "Build Mac OSX Plugin for architecture ${CMAKE_OSX_ARCHITECTURES}")
   add_compile_definitions(ARCH_MAC)
   if (${CMAKE_OSX_ARCHITECTURES} MATCHES "x86_64")
-    add_compile_definitions(ARCH_X64)
     add_compile_options(-arch x86_64 -march=nehalem)
   endif ()
   if (${CMAKE_OSX_ARCHITECTURES} MATCHES "arm64")
-    add_compile_definitions(ARCH_ARM64)
     add_compile_options(-arch arm64 -march=armv8-a+fp+simd)
     add_compile_options(-faligned-allocation)
   endif ()
 endif ()
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-  add_compile_definitions(ARCH_LIN ARCH_X64)
+  add_compile_definitions(ARCH_LIN)
   # This prevents static variables in the DSO (dynamic shared object) from being preserved after dlclose().
   add_compile_options(-fno-gnu-unique)
   # When Rack loads a plugin, it symlinks /tmp/Rack2 to its system dir, so the plugin can link to libRack.
